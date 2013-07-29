79150d1fd94cd73de3b8b74b2e69c0fc3115a941
Marc Friederich <marc@antistatique.net>
2013-07-29 23:24:49 +0200

    deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+set :application, "num3"
+set :repository,  "git@github.com:zufrieden/num3.git"
+set :scm,         :git
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to,   "/home/mfh/www/num3.ch/"
+set :user,        "mfh"
+set :domain,      "antistatique"
+set :use_sudo,    false
+
+set :ssh_options, { :forward_agent => true }
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+set :use_composer, true
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
